02c234799edfa01e409bfe7e2fbd9432c153817c
henrique <henrique560983@gmail.com>
2017-01-19 16:13:08 +0000

    final

--- app/src/main/java/com/example/gistree/db_con/application/controllers/ButtonControllers.java
+++ app/src/main/java/com/example/gistree/db_con/application/controllers/ButtonControllers.java
@@ -23,40 +23,40 @@ public class ButtonControllers implements View.OnClickListener {
 
     @Override
     public void onClick(final View v) {
-        HttpRequest req;
-        switch (v.getId()){
-            case R.id.btEcho:
-//                req = new HttpRequest(v.getContext(), "echo/Estou_Vivo", "GET");
-//                new HttpConnection(new HttpConnection.HttpResponse() {
-//                    @Override
-//                    public void serverResponse(String st) {
-//                        Toast.makeText(v.getContext(),st, Toast.LENGTH_SHORT).show();
-//                    }
-//                }).execute(req);
-                break;
-            case R.id.btEnviar:
-                RecordArvore arv1 = new RecordArvore();
-                arv1.setId(1);
-                arv1.setSpecies("Pinus");
-                RecordArvore arv2 = new RecordArvore();
-                arv2.setId(2);
-                arv2.setSpecies("Pinus2");
-                ArrayList<RecordArvore> arvs = new ArrayList<>();
-                arvs.add(arv1);
-                arvs.add(arv2);
-                RepositoryTimestamp tdf = new RepositoryTimestamp(context);
-                RecordTimestamp tsm = tdf.getLastTimestamp();
-//                req = new HttpRequest(v.getContext(), "post_test", "POST", tsm.getTimestamp(), arvs);
-//                new HttpConnection(new ServerAsyncResponse() {
-//                    @Override
-//                    public void serverResponse(String st) {
-//                        Toast.makeText(v.getContext(), st, Toast.LENGTH_SHORT).show();
-//                    }
-//                }).execute(req);
+//        HttpRequest req;
+//        switch (v.getId()){
+//            case R.id.btEcho:
+////                req = new HttpRequest(v.getContext(), "echo/Estou_Vivo", "GET");
+////                new HttpConnection(new HttpConnection.HttpResponse() {
+////                    @Override
+////                    public void serverResponse(String st) {
+////                        Toast.makeText(v.getContext(),st, Toast.LENGTH_SHORT).show();
+////                    }
+////                }).execute(req);
 //                break;
-            default:
-
-                break;
-        }
+//            case R.id.btEnviar:
+//                RecordArvore arv1 = new RecordArvore();
+//                arv1.setId(1);
+//                arv1.setSpecies("Pinus");
+//                RecordArvore arv2 = new RecordArvore();
+//                arv2.setId(2);
+//                arv2.setSpecies("Pinus2");
+//                ArrayList<RecordArvore> arvs = new ArrayList<>();
+//                arvs.add(arv1);
+//                arvs.add(arv2);
+//                RepositoryTimestamp tdf = new RepositoryTimestamp(context);
+//                RecordTimestamp tsm = tdf.getLastTimestamp();
+////                req = new HttpRequest(v.getContext(), "post_test", "POST", tsm.getTimestamp(), arvs);
+////                new HttpConnection(new ServerAsyncResponse() {
+////                    @Override
+////                    public void serverResponse(String st) {
+////                        Toast.makeText(v.getContext(), st, Toast.LENGTH_SHORT).show();
+////                    }
+////                }).execute(req);
+////                break;
+//            default:
+//
+//                break;
+//        }
     }
 }
